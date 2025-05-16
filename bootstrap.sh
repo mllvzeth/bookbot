40f9f57bf1434db21587eb8e87468a8cec9fd957
marcos <marcos@llavez.com>
2025-05-16 19:59:37 +0000

    update readme with a description

---
+++ bootstrap.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+cp .tmux.conf ~
+cp .gitconfig ~
+
+# ~Manually install NeoVim
+mkdir -p ~/.local
+cd ~/.local
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.appimage
+chmod u+x nvim-linux-x86_64.appimage
+./nvim-linux-x86_64.appimage --appimage-extract
+mkdir ~/.local/bin
+ln -s ~/.local/squashfs-root/AppRun ~/.local/bin/nvim
+
+# Bring in our custom neovim config
+echo "XDG_CONFIG_HOME=$HOME" >> ~/.profile
+git clone https://github.com/shea-parkes/neovim-config ~/.config/nvim
+cd ~/.config/nvim
+git submodule init
+git submodule update
+
+# Get Neovim mostly ready to go
+cd /workspaces/$RepositoryName
+# poetry run pip install pynvim ipython  # Avoid if we can help it
+poetry run nvim --headless +":UpdateRemotePlugins" +"q!"
+
+# Go ahead and configure vim as well while we're at it
+git clone https://github.com/shea-parkes/vim-config ~/.vim
+cd ~/.vim
+git submodule init
+git submodule update
+
+# Setup git completions for bash
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
+echo "source ~/.git-completion.bash" >> ~/.bashrc
